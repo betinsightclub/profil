ce2543f99fc123206f081e638a3749773693ad04
betinsightclub <betinsight.club@gmail.com>
2026-09-25 23:04:39 +0200

    Use clean 144px player face sprite grid

--- time-clash/avatar-facepack-v4.js
+++ time-clash/avatar-facepack-v4.js
@@ -4,20 +4,20 @@ const A=window.TimeClashAvatarAssets;
 const B64=window.__TC_FACEPACK_V4||"";
 if(!A||!B64){return}
 const META={
-"faces/player-m04":{x:0,y:0,w:128,h:128},
-"faces/player-m05":{x:128,y:0,w:128,h:128},
-"faces/player-m06":{x:256,y:0,w:128,h:128},
-"faces/player-m07":{x:384,y:0,w:128,h:128},
-"faces/player-m08":{x:0,y:128,w:128,h:128},
-"faces/player-m09":{x:128,y:128,w:128,h:128},
-"faces/player-m10":{x:256,y:128,w:128,h:128},
-"faces/player-m11":{x:384,y:128,w:128,h:128},
-"faces/player-m12":{x:0,y:256,w:128,h:128},
-"faces/player-m13":{x:128,y:256,w:128,h:128},
-"faces/player-m14":{x:256,y:256,w:128,h:128},
-"faces/player-m15":{x:384,y:256,w:128,h:128},
-"faces/player-m16":{x:0,y:384,w:128,h:128},
-"faces/player-m17":{x:128,y:384,w:128,h:128}
+"faces/player-m04":{x:0,y:0,w:144,h:144},
+"faces/player-m05":{x:144,y:0,w:144,h:144},
+"faces/player-m06":{x:288,y:0,w:144,h:144},
+"faces/player-m07":{x:432,y:0,w:144,h:144},
+"faces/player-m08":{x:0,y:144,w:144,h:144},
+"faces/player-m09":{x:144,y:144,w:144,h:144},
+"faces/player-m10":{x:288,y:144,w:144,h:144},
+"faces/player-m11":{x:432,y:144,w:144,h:144},
+"faces/player-m12":{x:0,y:288,w:144,h:144},
+"faces/player-m13":{x:144,y:288,w:144,h:144},
+"faces/player-m14":{x:288,y:288,w:144,h:144},
+"faces/player-m15":{x:432,y:288,w:144,h:144},
+"faces/player-m16":{x:0,y:432,w:144,h:144},
+"faces/player-m17":{x:144,y:432,w:144,h:144}
 };
 let url="";
 function faceSpriteUrl(){
@@ -32,7 +32,7 @@ A.cell=function(key){return META[key]||oldCell(key)};
 A.svgImage=function(key,x,y,w,h,extra=""){
  const m=META[key];
  if(!m)return oldSvg(key,x,y,w,h,extra);
- return `<svg x="${x}" y="${y}" width="${w}" height="${h}" viewBox="${m.x} ${m.y} ${m.w} ${m.h}" preserveAspectRatio="xMidYMid meet" ${extra}><image href="${faceSpriteUrl()}" x="0" y="0" width="512" height="512" preserveAspectRatio="none"/></svg>`;
+ return `<svg x="${x}" y="${y}" width="${w}" height="${h}" viewBox="${m.x} ${m.y} ${m.w} ${m.h}" preserveAspectRatio="xMidYMid meet" ${extra}><image href="${faceSpriteUrl()}" x="0" y="0" width="576" height="576" preserveAspectRatio="none"/></svg>`;
 };
 A.meta=Object.assign(A.meta||{},META);
 A.keys=Array.from(new Set([...(A.keys||[]),...Object.keys(META)]));
